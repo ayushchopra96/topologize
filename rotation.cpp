f767238f8b25a5cc4c2c9c3dcb476904f10e7ba5
ayush chopra <ayushchopra96@gmail.com>
2016-08-22 21:08:35 +0530

    Rotation module updated

--- rotation.cpp
+++ rotation.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 float magnituide(struct vectr v)//magnituide of a vector
 {
-   return sqrt(v.x*v.x+v.y*v.y+v.z*v.z);
+   float mag = sqrt(v.x*v.x+v.y*v.y+v.z*v.z);
+   return mag;
 }
 struct vectr unitVector(struct vectr v)
 {
